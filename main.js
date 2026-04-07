44756d7c787c685591a8d8870aa72dedecd37673
Penas9S-ay2526 <penas.kr@obmontessori.edu.ph>
2026-04-07 08:50:39 +0800

    edits

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
 function toSubmit(){
-    u = "SP_Admin";
-    p = "1892mtmghrer";
+    const user = "SP_Admin";
+    const pass = "1892mtmghrer";
 
     let userInput = document.getElementById("username").value;
     let passInput = document.getElementById("password").value;
-    let f = document.getElementById("feedback").value;
 
-    if (u === userInput && p === passInput) {
-        document.getElementById("feedback").innerHTML='Access Granted! Welcome, SP_Admin';}
-    else (u != userInput && p != passInput); {
-        document.getElementById("feedback").innerHTML='Access Denied. Please try again.'};
+    if (user === userInput && pass === passInput) {document.getElementById("feedback").innerHTML="Access granted! Welcome.";}
+    else; {document.getElementById("feedback").innerHTML="Access Denied. Please try again."};
 }
